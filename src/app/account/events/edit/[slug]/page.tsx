36f5c2bb9380006df624e4eee443f01807aced30
Shehab Waleed <shehabwaleedd@gmail.com>
2024-12-04 12:54:28 +0200

    build fixes.

--- src/app/account/events/edit/[slug]/page.tsx
+++ src/app/account/events/edit/[slug]/page.tsx
@@ -1,18 +1,20 @@
 import { type Metadata } from 'next';
 import Client from './(components)/Client';
 
-interface Props {
-    params: { slug: string };
-    searchParams: { [key: string]: string | string[] | undefined };
-}
+type Props = {
+    params: {
+        slug: string;
+    };
+    searchParams?: Record<string, string | string[] | undefined>;
+};
 
 export const metadata: Metadata = {
     title: 'Edit Event',
     description: 'Edit your event details',
 };
 
-export default async function EditEventPage({ params }: Props) {
-    const slug = params.slug;
 
-    return <Client slug={slug} />;
+export default async function EditEventPage({ params }: Props) {
+
+    return <Client slug={params.slug} />;
 }
